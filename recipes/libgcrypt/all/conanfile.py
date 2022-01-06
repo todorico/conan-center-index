bc19bb5a280fd31eed8b63a618f224b4335349bb
t.odorico <t.odorico@sim-and-cure.com>
2022-01-06 16:43:39 +0100

    Add libgcrypt recipe for Windows msys2

--- recipes/libgcrypt/all/conanfile.py
+++ recipes/libgcrypt/all/conanfile.py
@@ -1,10 +1,17 @@
 import os
-from conans import ConanFile, AutoToolsBuildEnvironment, tools
+from conans import ConanFile, tools
 from conans.errors import ConanInvalidConfiguration
+
+from conan.tools.gnu import Autotools, AutotoolsToolchain
+from conan.tools.files import apply_conandata_patches
+from conan.tools.env import Environment
+
 
 required_conan_version = ">=1.33.0"
 
+
 class LibgcryptConan(ConanFile):
+    # version = "1.8.4"
     name = "libgcrypt"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://www.gnupg.org/download/index.html#libgcrypt"
@@ -21,63 +28,78 @@ class LibgcryptConan(ConanFile):
         "fPIC": True,
     }
 
+    win_bash = True
     _autotools = None
 
     @property
     def _source_subfolder(self):
         return "source_subfolder"
 
-    def configure(self):
+    def config_options(self):
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
         if self.options.shared:
             del self.options.fPIC
 
     def validate(self):
-        if self.settings.os != "Linux":
-            raise ConanInvalidConfiguration("This recipe only support Linux. You can contribute Windows and/or Macos support.")
-
-    def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        if self.settings.os != "Linux" and self.settings.os != "Windows":
+            raise ConanInvalidConfiguration(
+                "This recipe only support Linux and Windows subsystems. You can contribute Macos support."
+            )
 
     def requirements(self):
-        self.requires("libgpg-error/1.36")
-        self.requires("libcap/2.50")
+        self.requires("libgpg-error/1.36@sim-and-cure/stable")
+        # self.requires("libcap/2.50")
 
-    def _configure(self):
-        if self._autotools:
-            return self._autotools
-        args = ["--disable-doc"]
-        args.append("--with-libgpg-error-prefix={}".format(self.deps_cpp_info["libgpg-error"].rootpath))
-        if self.options.get_safe("fPIC", True):
-            args.append("--with-pic")
-        if self.options.shared:
-            args.extend(["--disable-static", "--enable-shared"])
-        else:
-            args.extend(["--disable-shared", "--enable-static"])
+    def source(self):
+        tools.get(
+            **self.conan_data["sources"][self.version],
+            destination=self._source_subfolder,
+            strip_root=True,
+        )
 
-        self._autotools = AutoToolsBuildEnvironment(self)
-        self._autotools.configure(args=args, configure_dir=self._source_subfolder)
-        return self._autotools
+    def _maybe_msys2_path(self, path):
+        return (
+            tools.unix_path(path, tools.MSYS2)
+            if self.settings.os == "Windows"
+            else path
+        )
+
+    def generate(self):
+        tc = AutotoolsToolchain(self)
+        # tc.default_configure_install_args = True # not working in dev workflow maybe BUG ?
+
+        libgpg_error = self.dependencies["libgpg-error"]
+        tc.configure_args.extend(
+            [
+                f"--with-gpg-error-prefix={self._maybe_msys2_path(libgpg_error.package_folder)}",
+                f"--prefix={self._maybe_msys2_path(self.install_folder)}/package",
+                "--disable-doc",
+            ]
+        )
+        tc.generate()
 
     def build(self):
-        env_build = self._configure()
-        env_build.make()
+        autotools = Autotools(self)
+        autotools.configure(self._source_subfolder)
+        autotools.make()
+        autotools.install()
 
     def package(self):
-        env_build = self._configure()
-        env_build.install()
+        self.copy("*", dst="", src="package")
         self.copy(pattern="COPYING*", dst="licenses", src=self._source_subfolder)
         tools.remove_files_by_mask(os.path.join(self.package_folder, "lib"), "*la")
         tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
         tools.rmdir(os.path.join(self.package_folder, "share"))
 
     def package_info(self):
-        self.cpp_info.libs = ["gcrypt"]
-        bin_path = os.path.join(self.package_folder, "bin")
-        self.output.info("Appending PATH env var with : {}".format(bin_path))
-        self.env_info.PATH.append(bin_path)
+        self.cpp_info.libs = tools.collect_libs(self)
         self.cpp_info.names["pkg_config"] = "gcrypt"
-        if self.settings.os in ["Linux", "FreeBSD"]:
-            self.cpp_info.system_libs = ["pthread"]
+
+        # self.cpp_info.libs = ["gcrypt"]
+        # bin_path = os.path.join(self.package_folder, "bin")
+        # self.output.info("Appending PATH env var with : {}".format(bin_path))
+        # self.env_info.PATH.append(bin_path)
+        # self.cpp_info.names["pkg_config"] = "gcrypt"
+        # if self.settings.os in ["Linux", "FreeBSD"]:
+        #     self.cpp_info.system_libs = ["pthread"]
--- recipes/libgcrypt/all/test_package/test_package.c
+++ recipes/libgcrypt/all/test_package/test_package.c
@@ -1,10 +1,14 @@
 #include "gcrypt.h"
+#include "stdio.h"
 
 int main() {
+  printf("[TEST] gcrypt_md_open should work: ");
   gcry_md_hd_t handle;
   gcry_error_t result = gcry_md_open(&handle, 0, GCRY_MD_FLAG_HMAC);
   if (result) {
+      printf("FAILURE\n");
       return result;
   }
+  printf("SUCCESS\n");
   return 0;
 }
