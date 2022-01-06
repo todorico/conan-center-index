49cb607b6e8d2c7bb008e254faa0b469963206e9
t.odorico <t.odorico@sim-and-cure.com>
2022-01-06 14:12:23 +0100

    Add libgpg-error recipe for windows

--- recipes/libgpg-error/all/test_package/conanfile.py
+++ recipes/libgpg-error/all/test_package/conanfile.py
@@ -1,10 +1,28 @@
 import os
+import shutil
 
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, tools
+from conan.tools.cmake import CMake, CMakeToolchain
+from conan.tools.layout import cmake_layout
 
-class TestPackageConan(ConanFile):
+
+class TestPackage(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "CMakeDeps", "VirtualBuildEnv", "VirtualRunEnv"
+
+    @property
+    def _package(self):
+        return self.deps_cpp_info["libgpg-error"]
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["_package_name"] = self._package.name
+        tc.variables["_package_version"] = self._package.version
+        tc.preprocessor_definitions["_package_version"] = self._package.version
+        tc.generate()
 
     def build(self):
         cmake = CMake(self)
@@ -12,6 +30,21 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self.settings):
-            bin_path = os.path.join("bin", "test_package")
-            self.run(bin_path, run_environment=True)
+        if not tools.cross_building(self):
+            test = os.path.join(self.cpp.build.bindirs[0], "test_package")
+            self.run(test, env="conanrun")
+        self._remove_artifacts()
+
+    #####################################################################################
+
+    def _remove_artifacts(self):
+        for file in [
+            "conan.lock",
+            "conanbuildinfo.txt",
+            "conaninfo.txt",
+            "graph_info.json",
+        ]:
+            file_path = os.path.join(self.folders.source_folder, file)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+        shutil.rmtree(self.folders.build_folder, ignore_errors=True)
--- recipes/libgpg-error/all/test_package/test_package.c
+++ recipes/libgpg-error/all/test_package/test_package.c
@@ -1,6 +1,6 @@
 #include "gpg-error.h"
 
 int main() {
-  gpgrt_printf("");
+  gpgrt_printf("[TEST] gpg-error printf should work: SUCCESS");
   return 0;
 }
